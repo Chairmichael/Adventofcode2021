4ec23dc2d53cec2f5548f72ddd238c11543a8f40
Jefferson V. Henry <jeffvhenry@gmail.com>
2021-12-10 11:44:54 -0600

    fa

--- Day 9/part2.py
+++ Day 9/part2.py
@@ -8,9 +8,7 @@ def slt(n, i, data):
 # for el: scan horizontally and then vertically
 def main():
 	data = [[int(x) for x in row] for row in open('input.txt').read().split()]
-	with open('output.txt','w') as output:
-		for line in data:
-			output.write(''.join('.' if x != 9 else 'X' for x in line) + '\n')
+	
 
 if __name__ == '__main__':
 	main()
